343ab2ba3f0b2e2dadab7934fb90f688c311b21d
mansiv-business <mansivbusiness@gmail.com>
2025-12-25 07:23:44 +0600

    Update alertmessage.tsx

--- src/components/alertmessage.tsx
+++ src/components/alertmessage.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import * as motion from "motion/react-client";
 import { AnimatePresence } from "motion/react";
-import { Poppins } from "next/font/google";
+
 
 export default function AlertPopup() {
   const [isVisible, setIsVisible] = useState(false);
@@ -17,6 +17,7 @@ export default function AlertPopup() {
     <AnimatePresence>
       {isVisible && (
         <motion.div
+          className="alert-message"
           initial={{ opacity: 0, scale: 0.5, y: -50 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           exit={{ opacity: 0, scale: 0.5, y: -20 }}
@@ -24,17 +25,15 @@ export default function AlertPopup() {
             position: "fixed",
             top: "20px",
             background: "linear-gradient(135deg, #314a80ff, #446ca0ff)",
-            padding: "20px",
-            borderRadius: "12px",
+            borderRadius: "1.2rem",
             color: "white",
             boxShadow: "0 10px 25px rgba(0,0,0,0.2)",
             zIndex: 1000,
             display: "flex",
             alignItems: "center",
-            gap: "15px"
           }}
         >
-            <p style={{fontSize: "1.5rem"}}>Disclaimer: This site is only to show my work.</p>
+            <p>Mansiv: This site is only to show my work.</p>
           <button 
             onClick={() => setIsVisible(false)}
             style={{ background: "rgba(255, 255, 255, 0.2)", border: "none", color: "white", cursor: "pointer", borderRadius: "50%", padding: "5px 10px" }}
